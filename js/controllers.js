a1d0b21c53a1a4d208c7433039bee0a4eaa3a50c
Kerry Hatcher <kwhatcher@gmail.com>
2015-01-31 23:52:07 -0500

    init

---
+++ js/controllers.js
@@ -0,0 +1,20 @@
+/**
+ * Created by kwhatcher on 1/29/15.
+ */
+
+
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+
+    $scope.$apply()
+
+    $scope.location = "test"
+});
+
+
+
+
+
